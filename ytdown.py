8b1e08642fdd7d719ecdae58837d2aa6012b631c
ASwedishGamer <41468583+ASwedishGamer@users.noreply.github.com>
2022-11-07 13:41:56 +0100

    first commit

---
+++ ytdown.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+
+link = input("Link:")
+location = input("Location:")
+yt = YouTube(link)
+
+print("Title:", yt.title)
+
+yd = yt.streams.get_highest_resolution()
+yd.download(location)
