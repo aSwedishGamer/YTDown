b1bf7e4a6329bc05d4a172b4152e5e93e43fad31
A Swedish Gamer <41468583+ASwedishGamer@users.noreply.github.com>
2022-11-07 16:10:22 +0100

    Added spaces after "prints"
    to clean up user experience

--- ytdown.py
+++ ytdown.py
@@ -1,10 +1,10 @@
 from pytube import YouTube
 
-link = input("Link:")
-location = input("Location:")
+link = input("Link: ")
+location = input("Location: ")
 yt = YouTube(link)
 
-print("Title:", yt.title)
+print("Title: ", yt.title)
 
 yd = yt.streams.get_highest_resolution()
 yd.download(location)
