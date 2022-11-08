8007c990639683605463a29fb0e82c366e457cce
A Swedish Gamer <41468583+ASwedishGamer@users.noreply.github.com>
2022-11-08 14:13:54 +0100

    Added .webm file format for export

--- ytdown.py
+++ ytdown.py
@@ -1,10 +1,21 @@
 from pytube import YouTube
 
 link = input("Link: ")
-location = input("Location: ")
 yt = YouTube(link)
+print("Title:", yt.title)
 
-print("Title: ", yt.title)
+fileSuccess = 0
+while fileSuccess == 0:
+    filetype = str(input("Filetype (mp4 or webm): "))
+    if filetype == "mp4":
+        fileSuccess = fileSuccess + 1
+        filedown = yt.streams.get_highest_resolution()
+    elif filetype == "webm":
+        fileSuccess = fileSuccess + 1
+        filedown = yt.streams.get_by_itag(251)
+    else:
+        print("Not one of the available filetypes(mp4 or webm), try again")
 
-yd = yt.streams.get_highest_resolution()
-yd.download(location)
+location = input("Download Location: ")
+
+filedown.download(location)
